4363ceed183ab6066cc46bda41600961564ae717
Mwininia-Saanyeh <ramoakidas@gmail.com>
2022-08-17 18:08:02 +0000

    created react form

---
+++ src/components/ProfileForm.js
@@ -0,0 +1,56 @@
+import React, {useState} from 'react'
+
+function ProfileForm() {
+const {profile, setProfile} = useState({
+    firstname:"",
+    lastname:"",
+    email:"",
+    phone:"",
+});
+
+const handler = (event) => {
+    setProfile((prev) => ({
+      ...prev,
+       [event.target.name]: event.target.value
+    }));
+
+}
+const handleForm = (e) => {
+    e.preventDefault();
+    Submit(profile)
+}
+
+ return (
+    <div className='formContainer'>
+        <h3>Profile Form</h3>
+        <form>
+            <fieldset>
+                <legend>Bio data</legend> 
+                <div className='names'>
+                    <label>First Name
+                        <input name="firstname" value=
+                        {profile.firstname} type="text" onChange={handler}/>
+                    </label>
+                    <label>Last Name
+                        <input name="lastname" value=
+                        {profile.lastname} type="text" onChange={handler}/>
+                        
+                    </label>
+                </div>
+                <div className='names'>
+                    <label>Email
+                        <input name="email" value=
+                        {profile.email} type="text" onChange={handler}/>
+                    </label>
+                    <label>Phone
+                        <input name="phone" value=
+                        {profile.phone} type="text" onChange={handler}/>
+                    </label>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+  )
+}
+
+export default ProfileForm
